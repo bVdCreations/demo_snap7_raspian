6b9a421e0243784e290756d3d85f100eb0a13dbc
demo siemens raspian <bastiaan.vandenabeele@agsolutions.es>
2018-04-16 15:56:05 +0000

    set data structure

--- Communiation_PLC.py
+++ Communiation_PLC.py
@@ -16,13 +16,13 @@ class PLC:
         self._IP = ip_adress
         self._rack = rack
         self._slot = slot
-        self._DB_list = dict()
+        self._db_list = dict()
         self.create_db_inst_from_excell()
         try:
             self._connect = self._plc.connect(self._IP, rack, slot)
         except ConnectionError:
             print('no connection') # for test
-            # raise ConnectionError("Can not connect to the PLC")
+            raise ConnectionError("Can not connect to the PLC")
 
     def get_ip(self):
         return self._IP
@@ -49,139 +49,201 @@ class PLC:
     def create_db_inst_from_excell(self, file_name='DBs_PLC_300.xlsx', file_dir=''):
 
         for key, values in ReadDB_Data(file_name=file_name, file_dir=file_dir).read_data().items():
-            self._DB_list.update({key: DB_PLC(key, values)})
+            self._db_list.update({key: self.DB_PLC(key, values)})
 
+    def read_db(self, *args):
 
-class DB_PLC(snap7.util.DB):
+        if args is None:
+            dbs= self._db_list.keys()
+        else:
+            dbs=args
 
-    def __init__ (self, db_name, dict_varia):
-        self._db_name = db_name
-        self._list_variables = dict()
-        self._create_intc_variables(dict_varia)
-        self._db_read_data = bytearray()
-        self._db_write_data = bytearray()
+        db_values= dict()
+        for db in dbs:
+            db_instance = self._db_list[db]
+            db_instance.set_read_data(self._plc.db_get(int(db[2::])))
+            db_values.update({db: db_instance.read_variable()})
 
-    def _create_intc_variables(self, dict_varia):
-        for key, items in dict_varia.items():
-            if 'Comment' in items.keys():
-                comment = items['Comment']
+        return db_values
+
+    def read_variable(self, **kwargs):
+        pass
+
+    class DB_PLC:
+
+        def __init__(self, db_name, dict_varia):
+
+            self._db_name = db_name
+            self._list_variables = dict()
+            self._create_variables(dict_varia)
+            self._db_read_data = bytearray()
+            self._db_write_data = bytearray()
+
+        def _create_variables(self, dict_varia):
+            for key, items in dict_varia.items():
+                if 'Comment' in items.keys():
+                    comment = items['Comment']
+                else:
+                    comment = ''
+                self._list_variables.update(
+                    {key: self.DBvariables(items['Name'], items['Adress'], items['Type'],
+                                      items['Initial value'], comment)}
+                )
+
+        def get_name(self):
+            return self._db_name
+
+        def get_read_data(self):
+            return self._db_read_data
+
+        def set_read_data(self, read_data):
+            self._db_read_data = read_data
+
+        def get_write_data(self):
+            return self._db_write_data
+
+        def set_write_data(self, write_data):
+            self._db_write_data = write_data
+
+        def update_variables(self, update_dict):
+            for key , value in update_dict.items():
+                self._list_variables.update({key:value})
+
+        def read_variable(self, *args):
+
+            if len(args) == 0:
+                variables = self._list_variables.keys()
             else:
-                comment = ''
-            self._list_variables.update(
-                {key: DBvariables(items['Name'], items['Adress'], items['Type'],
-                                  items['Initial value'], comment)}
-            )
+                variables = args
 
-    def read_db(self):
-        self._db_read_data = self._plc.db_get(int(self._db_name[2::]))
-
-    def read_all(self):
-        self.read_db()
-        for key,value in self._list_variables.items():
-            print('{}   =    {}      type={}'.format(key,value.read_var(), value._variable_type))
+            var_values = dict()
+            for variable in variables:
+                var_inst = self._list_variables[variable]
+                var_inst.set_var_read(self.get_read_data())
+                var_values.update({variable: var_inst.read_var()})
 
 
-class DBvariables(snap7.util.DB, ):
+        class DBvariables:
 
-    def __init__(self, variable_name , variable_adress, variable_type,
-                 variable_init_value, variable_value=None, variable_comment=''):
+            def __init__(self, variable_name , variable_adress, variable_type,
+                         variable_init_value, variable_value=None, variable_comment=''):
 
-        self._variable_name = variable_name
-        self._variable_adress = convert_adress(variable_adress)
-        self._variable_type = variable_type
-        self._variable_init_value = variable_init_value
-        self._variable_value = variable_value
-        self._variable_comment = variable_comment
-        self._variable_read_bytearray = bytearray()
-        self._variable_write_bytearray = bytearray()
-        self._offset = 0
+                self._variable_name = variable_name
+                self._variable_adress = convert_adress(variable_adress)
+                self._variable_type = variable_type
+                self._variable_init_value = variable_init_value
+                self._variable_value = variable_value
+                self._variable_comment = variable_comment
+                self._variable_read_bytearray = bytearray()
+                self._variable_write_bytearray = bytearray()
+                self._offset = 0
 
-    def convert_init_value(self, init_value_string):
+            def convert_init_value(self, init_value_string):
 
-        if self._variable_type == "BOOL":
+                if self._variable_type == "BOOL":
 
-            if init_value_string == 'true':
-                return True
-            else:
-                return False
-        if self._variable_type =="REAL":
-            return float(init_value_string)
+                    if init_value_string == 'true':
+                        return True
+                    else:
+                        return False
+                if self._variable_type =="REAL":
+                    return float(init_value_string)
 
-        if self._variable_type =="BYTE":
+                if self._variable_type =="BYTE":
 
-            hex_string = (init_value_string.split('#')[-1])
-            if len(hex_string) == 1:
-                hex_string = '0' + hex_string
+                    hex_string = (init_value_string.split('#')[-1])
+                    if len(hex_string) == 1:
+                        hex_string = '0' + hex_string
 
-            return bytearray.fromhex(hex_string)
+                    return bytearray.fromhex(hex_string)
 
-        if self._variable_type =="WORD":
-            return init_value_string
+                if self._variable_type =="WORD":
+                    return init_value_string
 
-        if self._variable_type =="DWORD":
-            return init_value_string
+                if self._variable_type =="DWORD":
+                    return init_value_string
 
-        if self._variable_type =="INT":
-            return int(init_value_string)
+                if self._variable_type =="INT":
+                    return int(init_value_string)
 
-        if self._variable_type =="DINT":
-            return init_value_string
+                if self._variable_type =="DINT":
+                    return init_value_string
 
-        if self._variable_type =="S5TIME":
-            return init_value_string
+                if self._variable_type =="S5TIME":
+                    return init_value_string
 
-        if self._variable_type =="TIME":
-            return init_value_string
+                if self._variable_type =="TIME":
+                    return init_value_string
 
-        if self._variable_type =="DATE":
-            return init_value_string
+                if self._variable_type =="DATE":
+                    return init_value_string
 
-        if self._variable_type =="TIME _OF_DAY":
-            return init_value_string
+                if self._variable_type =="TIME _OF_DAY":
+                    return init_value_string
 
-        if self._variable_type =="CHAR":
-            return init_value_string
+                if self._variable_type =="CHAR":
+                    return init_value_string
 
-    def read_var(self):
+            def read_var(self):
 
-        if self._variable_type == "BOOL":
-            var = snap7.util.get_bool(self._db_read_data, self._variable_adress[0], self._variable_adress[1])
+                print("variable name = {}".format(self._variable_name))
+                print("variable type = {}".format(self._variable_type))
+                print("variable adress = {}".format(self._variable_adress))
 
-        if self._variable_type =="REAL":
 
-            var = snap7.util.get_real(self._db_read_data, self._variable_adress[0])
+                if self._variable_type == "BOOL":
+                    var = snap7.util.get_bool(self.get_var_read(), self._variable_adress[0], self._variable_adress[1])
 
-        if self._variable_type =="BYTE":
+                if self._variable_type =="REAL":
 
-            var = 'under constrution'
+                    var = snap7.util.get_real(self.get_var_read(), self._variable_adress[0])
 
-        if self._variable_type =="WORD":
-            var = 'under constrution'
+                if self._variable_type =="BYTE":
 
-        if self._variable_type =="DWORD":
-            var = 'under constrution'
+                    var = 'under constrution'
 
-        if self._variable_type =="INT":
-            var = snap7.util.get_int(self._db_read_data, self._variable_adress[0])
+                if self._variable_type =="WORD":
+                    var = 'under constrution'
 
-        if self._variable_type =="DINT":
-            var = 'under constrution'
+                if self._variable_type =="DWORD":
+                    var = 'under constrution'
 
-        if self._variable_type =="S5TIME":
-            var = 'under constrution'
+                if self._variable_type =="INT":
+                    var = snap7.util.get_int(self.get_var_read(), self._variable_adress[0])
 
-        if self._variable_type =="TIME":
-            var = 'under constrution'
+                if self._variable_type =="DINT":
+                    var = 'under constrution'
 
-        if self._variable_type =="DATE":
-            var = 'under constrution'
+                if self._variable_type =="S5TIME":
+                    var = 'under constrution'
 
-        if self._variable_type =="TIME _OF_DAY":
-            var = 'under constrution'
+                if self._variable_type =="TIME":
+                    var = 'under constrution'
 
-        if self._variable_type =="CHAR":
-            var = 'under constrution'
-        return var
+                if self._variable_type =="DATE":
+                    var = 'under constrution'
+
+                if self._variable_type =="TIME _OF_DAY":
+                    var = 'under constrution'
+
+                if self._variable_type =="CHAR":
+                    var = 'under constrution'
+
+                self._variable_value = var
+                return var
+
+            def get_var_read(self):
+
+                return self._variable_read_bytearray
+
+            def set_var_read(self, var_bytearray):
+                self._variable_read_bytearray= var_bytearray
+
+            def get_var_write(self):
+                return self._variable_write_bytearray
+
+            def set_var_read(self, var_bytearray):
+                self._variable_write_bytearray = var_bytearray
 
 
 def convert_adress(adress_string):
@@ -206,35 +268,7 @@ def convert_adress(adress_string):
     #     plc.write_area(areas["MK"],0,byte,result)
 
 
-
-
-
-# def ReadMemory(plc,byte,bit,datatype):
-#     result = plc.read_area(areas['MK'],0,byte,datatype)
-#     if datatype==S7WLBit:
-#         return get_bool(result,0,bit)
-#     elif datatype==S7WLByte or datatype==S7WLWord:
-#         return get_int(result,0)
-#     elif datatype==S7WLReal:
-#         return get_real(result,0)
-#     elif datatype==S7WLDWord:
-#         return get_dword(result,0)
-#     else:
-#         return None
-#
-# def WriteMemory(plc,byte,bit,datatype,value):
-#     result = plc.read_area(areas['MK'],0,byte,datatype)
-#     if datatype==S7WLBit:
-#         set_bool(result,0,bit,value)
-#     elif datatype==S7WLByte or datatype==S7WLWord:
-#         set_int(result,0,value)
-#     elif datatype==S7WLReal:
-#         set_real(result,0,value)
-#     elif datatype==S7WLDWord:
-#         set_dword(result,0,value)
-#     plc.write_area(areas["MK"],0,byte,result)
-
-
 if __name__ == '__main__':
     plc = PLC('10.34.0.95',rack=0,slot=2)
-    plc._DB_list['DB1'].read_all
+    print('test')
+    print(plc.read_db('DB2'))
