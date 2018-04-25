8a48e4efd56ca302250561b98937dc6f9491d303
demo siemens raspian <bastiaan.vandenabeele@agsolutions.es>
2018-04-25 14:06:39 +0000

    added functionallaty to read in symbol table from excell

--- Read_DB_Data_Excell.py
+++ Read_DB_Data_Excell.py
@@ -61,7 +61,41 @@ class ReadDB_Data:
         else:
             raise FileNotFoundError('"info_PLC" can not be found in the sheetnames : {}'.format(sheetnames))
 
+    def read_i_o(self):
+        """ returns a dict with the keys Inputs and Outputs
+        the values of those keys are the """
 
+        sheetnames = self._workbook.sheetnames
+        if 'Symbol_table' in sheetnames:
+
+            return_dict = {'Input': dict(), 'Output': dict()}
+
+            # loop through the rows of the sheet
+            sheet_object = self._workbook['Symbol_table']
+            last_entry = self.get_last_entry_sheet(sheet_object)
+            for rowOfCellObjects in sheet_object['A2':last_entry]:
+                symbol_name = rowOfCellObjects[0].value
+                adress_io = rowOfCellObjects[1].value
+
+                # check if its an input or output
+                if adress_io[0] == 'I':
+                    store = 'Input'
+                elif adress_io[0] == 'Q':
+                    store = 'Output'
+                else:
+                    raise ValueError('the adress "{}" should start with I or Q'.format(adress_io))
+
+                return_dict[store].update({symbol_name: dict()})
+
+                # loop through the cells in the row
+                for cellObj in rowOfCellObjects:
+                    
+                    if cellObj.value is not None:
+                        return_dict[store][symbol_name].update(
+                            {sheet_object.cell(column=cellObj.col_idx, row=1).value: cellObj.value})
+            return return_dict
+        else:
+            raise FileNotFoundError('"Symbol_table" can not be found in the sheetnames : {}'.format(sheetnames))
 
     def get_last_entry_sheet(self, sheet_object: openpyxl, offset=0):
         # find the maximum range of data in the sheet
@@ -100,4 +134,13 @@ if '__main__' == __name__:
         for keyvar, value in values.items():
             print('\tkeyvar ={}'.format(keyvar))
             print('\ttype ={}'.format(value['Type']))
+        print('-'*10)
+
+    print('='*20)
+    for i_or_o, values in ReadDB_Data('DBs_PLC_300.xlsx', file_dir='I_O_info_plc//').read_i_o().items():
+        print('key ={}'.format(i_or_o))
+
+        for keyvar, value in values.items():
+            print('\tkeyvar ={}'.format(keyvar))
+            print('\ttype ={}'.format(value['Data type']))
         print('-'*10)
